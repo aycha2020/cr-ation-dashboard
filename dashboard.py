664d7a9fd0f71c894a1e77f6e15f778a80621c42
unknown <aishahbb033@gmail.com>
2026-03-31 14:33:58 +0100

    update

--- dashboard.py
+++ dashboard.py
@@ -14,6 +14,27 @@ df = pd.read_csv("data.csv")
 
 st.title("Superstore Sales Dashboard")
 
+region = st.sidebar.multiselect(
+    "Select Region",
+    options=df["region"].unique(),
+    default=df["region"].unique()
+)
+
+category = st.sidebar.multiselect(
+    "Select Category",
+    options=df["category"].unique(),
+    default=df["category"].unique()
+)
+
+annee = st.sidebar.multiselect(
+    "Select Year",
+    options=df["annee"].unique(),
+    default=df["annee"].unique()
+)
+
+df_filtered=df[(df["region"].isin(region))
+                &(df["category"].isin(category))
+                &(df["annee"].isin(annee))]
 
 # Filter
 
@@ -23,7 +44,7 @@ region = st.sidebar.selectbox(
     df["region"].unique()
 )
 
-df_filtered = df[df["region"] == region]
+df_filtered1 = df[df["region"] == region]
 
 
 # KPIs
@@ -48,16 +69,56 @@ sales_region = df_filtered.groupby("region")["sales"].sum().reset_index()
 
 fig, ax = plt.subplots()
 
-sns.barplot(data=sales_region, x="region", y="sales", ax=ax)
+sns.barplot(data=sales_region, x="region", y="sales", ax=ax,color="#DEED08")
 
 ax.set_title("Sales by Region")
 
 st.pyplot(fig)
 
 
-# Show Data
 
+# Calcul des totaux par catégorie
+df_cat = df_filtered.groupby("category")[["sales", "profit"]].sum()
 
-st.subheader("Filtered Data")
+# Création du graphique
+fig, ax = plt.subplots(figsize=(8,5))
 
-st.write(df_filtered)
+df_cat.plot(
+    kind="bar",
+    ax=ax,
+    color=["#A408ED", "lightgreen"]
+)
+
+ax.set_title("Ventes vs Profit par catégorie")
+ax.set_xlabel("Catégorie")
+ax.set_ylabel("Montant (€)")
+ax.set_xticklabels(df_cat.index, rotation=0)
+ax.legend(["Ventes", "Profit"])
+
+# Affichage dans Streamlit
+st.pyplot(fig)
+
+ventes_categorie=df_filtered.groupby('category')["sales"].sum()
+colors = ["#DF1111", "#2905E1", "#0CD520"]
+
+# Création figure
+fig, ax = plt.subplots()
+
+# Graphe en secteurs
+
+ventes_categorie.plot(
+    kind="pie",
+    autopct="%1.1f%%",
+    colors=colors,
+    startangle=90,
+    shadow=True,
+    explode=[0.05]*len(ventes_categorie),
+    ax=ax
+)
+
+ax.set_title("Répartition des ventes par catégorie")
+ax.set_ylabel("")  # supprimer label
+ax.axis("equal")   # cercle parfait
+
+# Affichage Streamlit
+st.pyplot(fig)
